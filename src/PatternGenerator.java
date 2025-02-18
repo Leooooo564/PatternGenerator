f8a98eee8380b79b62b833430e73d8cabf3c1a70
Leooooo564 <lchristmann2@gmail.com>
2025-02-18 20:52:22 +0100

    Increase matrix size, update input neurons generation, and modify random selection range in PatternGenerator

--- src/PatternGenerator.java
+++ src/PatternGenerator.java
@@ -5,15 +5,16 @@ import java.io.IOException;
 
 public class PatternGenerator {
     static Random random = new Random();
-    private static int matrixSize = 10;
+    private static int matrixSize = 20;
     private static int patternAmout = 100;
     private static int[][] matrix = new int[matrixSize][matrixSize];
     private static String inputFilepath = "";
     private static String outputFilepath = "";
-    private static String inputNeurons = "1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;21;22;23;24;25;26;27;28;29;30;31;32;33;34;35;36;37;38;39;40;41;42;43;44;45;46;47;48;49;50;51;52;53;54;55;56;57;58;59;60;61;62;63;64;65;66;67;68;69;70;71;72;73;74;75;76;77;78;79;80;81;82;83;84;85;86;87;88;89;90;91;92;93;94;95;96;97;98;99;100";
-    private static String outputNeurons = "131;132;133";
+    private static String inputNeurons = inputNeurons();
+    private static String outputNeurons = "521;522;523";
 
     public static void main(String[] args) {
+
         inputFilepath = generateNewFile(true);
         outputFilepath = generateNewFile(false);
         printToFile(true, inputNeurons + "\n");
@@ -21,7 +22,7 @@ public class PatternGenerator {
 
         for (int i = 0; i < patternAmout; i++) {
             initializeMatrix();
-            int rand = random.nextInt(3);
+            int rand = random.nextInt(4);
             String outputPattern = "";
 
             switch (rand) {
@@ -42,7 +43,7 @@ public class PatternGenerator {
                     outputPattern = "0;0;0";
                     break;
             }
-            printMatrix();
+            // printMatrix();
             printToFile(true, tooString() + "\n");
             printToFile(false, outputPattern + "\n");
         }
@@ -54,6 +55,17 @@ public class PatternGenerator {
                 matrix[i][j] = 0;
             }
         }
+    }
+
+    private static String inputNeurons() {
+        String tmp = "";
+        for (int i = 1; i <= matrixSize * matrixSize; i++) {
+            tmp += i + ";";
+        }
+        if (tmp.length() > 0) {
+            tmp = tmp.substring(0, tmp.length() - 1);
+        }
+        return tmp;
     }
 
     private static void generateSquare() {
